498cd193fd5bac959d5cc4e98e4616738b28b8dd
Vasiliy Alferov /platypus/ <ya-ikmik2012@yandex.ru>
2016-01-01 17:28:23 +0300

    ADded classes

--- server.py
+++ server.py
@@ -8,4 +8,21 @@
     callback, который дёргают socket'ы:
     - на чувак подключился
     - на чувак что-то послал
-""
+"""
+
+import random
+
+
+class AgarioPlayer:
+    def __init__(self, name):
+        self.name = name
+        self.id = random.randint(1, 10 ** 36)
+        self.circles = [(random.randint(0, 8000), random.randint(0, 4000), 10)]
+    
+
+class AgarioServer:
+    def __init__(self):
+        """
+            call init у сокетов
+        """
+        self.players = []
