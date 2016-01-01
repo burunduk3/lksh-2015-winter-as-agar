443cbc810235d93cb80198f981d2f14ec3378ee6
sis-2015-65-2 <nefedov_as@mail.ru>
2016-01-01 17:46:24 +0300

    draw

---
+++ draw.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+from math import sqrt
+
+
+def draw_player(c, p):
+    c = tk.Canvas()
+    r = sqrt(p['mass'])
+    c.create_oval(p['x'] - r,
+                  p['y'] - r,
+                  p['x'] + r,
+                  p['y'] + r,
+                  fill=p['color']
+                  )
+    return c
