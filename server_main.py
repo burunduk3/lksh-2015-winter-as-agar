6da2a00e41afb7d76af108fd84274d73b62f055d
bixind <vbif-98@mail.ru>
2016-01-02 12:03:30 +0400

    some bugfix

--- server_main.py
+++ server_main.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from server import *
 import server_protocol as protocol
 
@@ -12,26 +14,25 @@ while True:
     now = time()
     if (lastTime + time_step < now):
         """
-            ÷àñòü ãäå âñ¸ îáíîâëÿåòñÿ
+            Ñ‡Ð°ÑÑ‚ÑŒ Ð³Ð´Ðµ Ð²ÑÑ‘ Ð¾Ð±Ð½Ð¾Ð²Ð»ÑÐµÑ‚ÑÑ
         """
         dt = now - lastTime
         lastTime = now
 
-        server.applUpdate(1)
+        server.applUpdate(0)
         cursors = []
         circles = []
-        for pl in server.players:
+        for pl in server.players.values():
             id = pl.id
             cursors.append({'x' : pl.cursor[0], 'y' : pl.cursor[1], 'id' : id})
             for circle in pl.circles:
-                circles.append({'x' : circles[0], 'y' : circle[1], 'm' : circle[2], 'id' : id})
+                circles.append({'x' : circle[0], 'y' : circle[1], 'm' : circle[2], 'id' : id})
         newcirlces = physics.updateMap0(cursors, circles, dt)
         server.updateCirlces(newcirlces)
         """
-            ðàññêàçàòü âñåì î íîâûõ ïîëÿõ
+            Ñ€Ð°ÑÑÐºÐ°Ð·Ð°Ñ‚ÑŒ Ð²ÑÐµÐ¼ Ð¾ Ð½Ð¾Ð²Ñ‹Ñ… Ð¿Ð¾Ð»ÑÑ…
         """
-        for player in server.players:
+        for player in server.players.values():
             protocol.sendMap(player.id, server.makeFieldMessage(player.id))
-
     else:
         sleep(0.01)
