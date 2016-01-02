4cd2d2248d3a81ac185b6f3630edf39086acef57
sis-2015-65-2 <nefedov_as@mail.ru>
2016-01-02 19:24:40 +0300

    fixed old_json

--- client_protocol.py
+++ client_protocol.py
@@ -1,45 +1,48 @@
 import json, socket, time
-                    
+
 sock = socket.socket()
 old_data = ""
 old_json = []
 
-def registerMe(name):     
+
+def registerMe(name):
     global sock, tcp_ip, tcp_port
 
     inf = open('config.txt', 'r')
     config = inf.readline()
     tcp_ip, tcp_port = config.split(' ')
     tcp_port = int(tcp_port)
-                                  
+
     sock.connect((tcp_ip, tcp_port))
     id = json.loads(str(sock.recv(1024), 'utf-8'))['id']
     jdata = dict()
     jdata['name'] = name
-    s = json.dumps(jdata) 
+    s = json.dumps(jdata)
     sock.send(bytes(s, 'utf-8'))
     return id
-             
+
+
 def getField():
-    global sock, old_data    
+    global sock, old_data, old_json
     data = sock.recv(1024)
     data = str(data, 'utf-8')
 
-    s = old_data+data            
+    s = old_data + data
     l = s.split('\n')
 
     old_data = l[-1]
     if len(l) > 1:
         old_json = json.loads(l[-2])
-    
-    return old_json               
+    return old_json
+
 
 def sendMe(p):
     global sock
     data = json.dumps(p)
     sock.send(bytes(data, 'utf-8'))
 
+
 def killMe():
     global sock
-    #sock.send(bytes('killme', 'utf-8'))
-    sock.close()  
+    # sock.send(bytes('killme', 'utf-8'))
+    sock.close()
