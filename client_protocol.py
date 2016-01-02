a4b25c6e835d71eca9fcc3a4c88ed6813df80b9a
GrigoryBartosh <grishabartosh@yandex.ru>
2016-01-02 10:58:24 +0300

    Create client_protocol.py

---
+++ client_protocol.py
@@ -0,0 +1,33 @@
+import json, socket, time
+
+tcp_ip = '192.168.3.49'
+tcp_port = 3030
+
+sock = socket.socket()
+
+def registerMe(name):     
+    global sock, tcp_ip, tcp_port        
+    sock.connect((tcp_ip, tcp_port))             
+    id = json.loads(str(sock.recv(1024), 'utf-8'))['id']
+    jdata = dict()
+    jdata['name'] = name
+    s = json.dumps(jdata) 
+    sock.send(bytes(s, 'utf-8'))
+    return id
+             
+def getField():
+    global sock    
+    data = sock.recv(1024)
+    data = str(data, 'utf-8')
+    return json.loads(data)
+
+def sendMe(p):
+    global sock
+    data = json.dumps(p)
+    print(data)   
+    sock.send(bytes(data, 'utf-8'))
+
+def killMe():
+    global sock
+    #sock.send(bytes('killme', 'utf-8'))
+    sock.close()
