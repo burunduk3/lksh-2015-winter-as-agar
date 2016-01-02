01219ba36fe0b7f88b57a1717476eeda63d2812b
bixind <vbif-98@mail.ru>
2016-01-02 12:25:53 +0400

    hz

--- server_main.py
+++ server_main.py
@@ -20,7 +20,6 @@ while True:
         """
         dt = now - lastTime
         lastTime = now
-
         server.applUpdate(0)
         cursors = []
         circles = []
@@ -29,12 +28,13 @@ while True:
             cursors.append({'x' : pl.cursor[0], 'y' : pl.cursor[1], 'id' : id})
             for circle in pl.circles:
                 circles.append({'x' : circle[0], 'y' : circle[1], 'm' : circle[2], 'id' : id})
-        newcirlces = physics.updateMap0(cursors, circles, dt)
+        newcirlces = physics.updateMap1(cursors, circles, dt)
         server.updateCirlces(newcirlces)
         """
             рассказать всем о новых полях
         """
         for player in server.players.values():
             protocol.sendMap(player.id, server.makeFieldMessage(player.id))
+        # print(now, )
     else:
         sleep(0.01)
