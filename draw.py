83df5ad4f12c80e5d8cf9b30805fab78377b610b
sis-2015-65-2 <nefedov_as@mail.ru>
2016-01-04 16:00:28 +0300

    fixed old_json

--- draw.py
+++ draw.py
@@ -1,18 +1,19 @@
 from math import sqrt
 from constants import *
 
-step = 25
+STEP = 25
+OUTLINE_WIDTH = 3
 
 
 def draw_bg(c, pos):
     # c - Canvas, pos - (x0, y0)
     x0, y0 = pos
-    x0 = step - x0 % step
-    y0 = step - y0 % step
-    for i in range(int(WINDOW_WIDTH / step) + 1):
-        c.create_line(i * step + x0, 0, i * step + x0, WINDOW_HEIGHT, fill='#D0D0D0')
-    for i in range(int(WINDOW_HEIGHT / step) + 1):
-        c.create_line(0, i * step + y0, WINDOW_WIDTH, i * step + y0, fill='#D0D0D0')
+    x0 = STEP - x0 % STEP
+    y0 = STEP - y0 % STEP
+    for i in range(int(WINDOW_WIDTH / STEP) + 1):
+        c.create_line(i * STEP + x0, 0, i * STEP + x0, WINDOW_HEIGHT, fill='#D0D0D0')
+    for i in range(int(WINDOW_HEIGHT / STEP) + 1):
+        c.create_line(0, i * STEP + y0, WINDOW_WIDTH, i * STEP + y0, fill='#D0D0D0')
     return c
 
 
@@ -21,29 +22,41 @@ def draw_players(c, pos, ps):
     x0, y0 = pos
     q = []
     for p in ps:
-        for b in p['balls']:
+        for ball in p['balls']:
             w = dict()
-            w['x'] = b['x']
-            w['y'] = b['y']
-            w['m'] = b['m']
+            w['x'] = ball['x']
+            w['y'] = ball['y']
+            w['m'] = ball['m']
             w['color'] = p['color']
             w['name'] = p['name']
             w['id'] = p['id']
             q.append(w)
     q.sort(key=lambda a: (a['m'], a['id']))
-    for b in q:
-        lx = b['x'] - x0
-        ly = b['y'] - y0
-        r = int(sqrt(b['m']))
-        c.create_oval(lx - r,
-                      ly - r,
-                      lx + r,
-                      ly + r,
-                      fill=b['color'],
+    for ball in q:
+        lx = ball['x'] - x0
+        ly = ball['y'] - y0
+        radius = int(sqrt(ball['m']))
+        r = int(ball['color'][1:3], 16)
+        g = int(ball['color'][3:5], 16)
+        b = int(ball['color'][5:7], 16)
+        r = max(0, r - 20)
+        g = max(0, g - 20)
+        b = max(0, b - 20)
+        c.create_oval(lx - radius - OUTLINE_WIDTH,
+                      ly - radius - OUTLINE_WIDTH,
+                      lx + radius + OUTLINE_WIDTH,
+                      ly + radius + OUTLINE_WIDTH,
+                      fill='#%02X%02X%02X' % (r, g, b),
                       outline='')
-        if len(b['name']) > 0 and r // len(b['name']) > 3:
+        c.create_oval(lx - radius,
+                      ly - radius,
+                      lx + radius,
+                      ly + radius,
+                      fill=ball['color'],
+                      outline='')
+        if len(ball['name']) > 0 and radius // len(ball['name']) > 3:
             c.create_text(lx, ly,
-                          font=('Comic Sans MS', 2 * r // (len(b['name']) + 1)),
-                          text=b['name'],
+                          font=('Comic Sans MS', 2 * radius // (len(ball['name']) + 1)),
+                          text=ball['name'],
                           fill='white')
     return c
