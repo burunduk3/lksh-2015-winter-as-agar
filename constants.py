6e1d92694743f00dffbcf726a61ba5653e9fe616
bixind <vbif-98@mail.ru>
2016-01-04 21:21:02 +0400

    test

--- constants.py
+++ constants.py
@@ -3,8 +3,8 @@ from math import sqrt
 time_step = 0.05
 INITIAL_MASS = 20
 
-FIELD_X = 3000
-FIELD_Y = 3000
+FIELD_X = 6000
+FIELD_Y = 500
 
 WINDOW_HEIGHT = 650
 WINDOW_WIDTH = 1300
@@ -23,7 +23,7 @@ DEBUG_OFF = False
 def debug(D):
     return (D | DEBUG_ON) & (not DEBUG_OFF)
 
-DEBUG_PROTOCOL = debug(False)
+DEBUG_PROTOCOL = debug(True)
 DEBUG_PROTOCOL_PRINT = debug(False)
 DEBUG_SERVER_PRINT = debug(False)
 
