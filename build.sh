3135909e7b9219775a970b1f469ff1adbd0cd702
Sergey Kopeliovich <Burunduk30@gmail.com>
2016-01-06 12:16:16 +0300

    add opengl-client to build script

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 suf=`echo 'import time; print(time.strftime("%y%m%d_%H"))' | python`
 client=dist/client_user/
+client2=dist/client_opengl/
 server=dist/server_main/
+
+pyinstaller client_opengl.py || exit
+cp config.txt $client2
 
 pyinstaller client_user.py || exit
 cp config.txt $client
@@ -8,6 +12,7 @@ cp config.txt $client
 pyinstaller server_main.py || exit
 cp config.txt $server
 
+mv $client2 dist/win-client2-$suf
 mv $client dist/win-client-$suf
 mv $server dist/win-server-$suf
 
